347bd0c05b81f1a2f4daf6cdb2fc65acf9043223
Robert Ramírez Vique <robertrv@gmail.com>
2013-07-11 14:57:57 +0200

    trying to fix travis-ci!

--- src/test/java/nl/flotsam/xeger/RegexGenerationSupport.java
+++ src/test/java/nl/flotsam/xeger/RegexGenerationSupport.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009 Robert Ramírez
+ * Copyright 2009 Wilfred Springer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
